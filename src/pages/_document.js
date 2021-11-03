e29dfe66181de84796ea8c901cd825a114967a53
Gustavo Daza <gustavo.dmartinez@hotmail.com>
2021-11-02 19:46:15 -0600

    Integración de Google Analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -32,9 +32,17 @@ export default class MyDocument extends Document {
         return (
             <Html {...this.helmetHtmlAttrComponents}>
                 <Head>{this.helmetHeadComponents}</Head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-XWGWPFDDR8"></script>
+                <script dangerouslySetInnerHTML={{
+                    __html: ` window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-XWGWPFDDR8');`
+                }} />
                 <body {...this.helmetBodyAttrComponents}>
                     <Main />
-                    <script src="/js/plugins.js" />
+                    <script src={withPrefix('js/plugins.js')} />
                     <NextScript />
                 </body>
             </Html>
